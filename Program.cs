4551d106fabde7fd0cadf8a29960e0f8db9c5b1f
Marius Aalto <108743532+aaltofar@users.noreply.github.com>
2024-05-30 13:04:53 +0200

    filechanges

--- Program.cs
+++ Program.cs
@@ -1,27 +1,44 @@
-using webApiTutorial.Components;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using webApiTutorial.Data;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+{
+    EnvironmentName = Environments.Development
+});
 // Add services to the container.
-
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.WebHost.UseStaticWebAssets();
-builder.Services.AddRazorComponents();
+
 var app = builder.Build();
 app.UseAntiforgery();
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// if (!app.Environment.IsDevelopment())
+// {
+app.UseExceptionHandler("/Error");
+// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+app.UseHsts();
+app.UseSwagger();
+app.UseSwaggerUI(options => // UseSwaggerUI is called only in Development.
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
+// }
 
 app.UseHttpsRedirection();
 
+app.UseStaticFiles();
 app.UseAuthorization();
-
+app.UseRouting();
 app.MapControllers();
-app.MapRazorComponents<App>();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
 app.Run();
--- obj/Debug/net8.0/webApiTutorial.AssemblyInfo.cs
+++ obj/Debug/net8.0/webApiTutorial.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webApiTutorial")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b530a510c16fb04c15c8b8ad9c9caa4e357939c")]
 [assembly: System.Reflection.AssemblyProductAttribute("webApiTutorial")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webApiTutorial")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
