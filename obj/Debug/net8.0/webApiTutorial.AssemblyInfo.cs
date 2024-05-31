84518b0b988b71683c772ab9c2f7a12b406a8e62
Marius Aalto <108743532+aaltofar@users.noreply.github.com>
2024-05-31 11:10:35 +0200

    jåddæ

--- obj/Debug/net8.0/webApiTutorial.AssemblyInfo.cs
+++ obj/Debug/net8.0/webApiTutorial.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webApiTutorial")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4551d106fabde7fd0cadf8a29960e0f8db9c5b1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95ab2bee42c66c2c2c9740f34ed5cea1aec36991")]
 [assembly: System.Reflection.AssemblyProductAttribute("webApiTutorial")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webApiTutorial")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
